32537c8a7cd352c6d5a6264abc58b8b69d494b35
Kosta Malsev <51965607+KostaMalsev@users.noreply.github.com>
2022-03-29 14:40:26 +0300

    Update app.js

--- web_app/js/app.js
+++ web_app/js/app.js
@@ -32,6 +32,18 @@ class RootComponent extends React.Component {
             }}
         />;
     }
+    
+    
+    //JSX part:
+  const element = ( <
+    h1 >
+    Hello, {
+      formatName(user)
+    }!
+    <
+    /h1>
+  );
+
 }
 
 
