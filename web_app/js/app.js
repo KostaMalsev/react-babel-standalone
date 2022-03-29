3079bd26d32ad3d9311064adbf08eeb04f6914aa
Kosta Malsev <51965607+KostaMalsev@users.noreply.github.com>
2022-03-29 14:49:26 +0300

    Update app.js

--- web_app/js/app.js
+++ web_app/js/app.js
@@ -35,6 +35,21 @@ class RootComponent extends React.Component {
 
 }
 
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
+}
+
+const user = {
+  firstName: 'Harper',
+  lastName: 'Perez'
+};
+
+const element = (
+  <h1>
+    Hello, {formatName(user)}!
+  </h1>
+);
+
 
 ReactDOM.render(
     <RootComponent />,
